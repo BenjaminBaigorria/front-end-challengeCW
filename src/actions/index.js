4d8a07910a63d3b48d8d6ee25798e3e670857306
BenjaminBaigorria <85437392+BenjaminBaigorria@users.noreply.github.com>
2022-07-28 08:21:38 -0300

    fixing bugs

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ export const SEND_TEXT="SEND_TEXT"
 
 
 export function sendText(text){
+    text=text.toLowerCase()
     return async function(dispatch){
         const txt= await axios.get(`https://back-challenge-copywritecol.herokuapp.com/iecho?text=${text}`)
         return dispatch({
